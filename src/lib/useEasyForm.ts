d2b1dc63413a744c6dc2b695eb03c695d8d90ae3
ArtemStepanov <artem.s@twsgo.com>
2025-09-03 15:23:41 +0300

    feat: test

--- src/lib/useEasyForm.ts
+++ src/lib/useEasyForm.ts
@@ -1,236 +1,242 @@
 import { useState, useRef, useCallback, useMemo } from 'react';
 
-import {
-  checkRequiredProperty,
-  compareValues,
-  getOutputObject,
-  getOtherValues,
-  hasAnyErrorsInForm,
-  checkFormValid,
-  multiUpdate,
-  transformArrayToObject,
-  validator,
-  setPropertiesToForm,
-} from './helpers';
+// import {
+//   checkRequiredProperty,
+//   compareValues,
+//   getOutputObject,
+//   getOtherValues,
+//   hasAnyErrorsInForm,
+//   checkFormValid,
+//   multiUpdate,
+//   transformArrayToObject,
+//   validator,
+//   setPropertiesToForm,
+// } from './helpers';
 
 import {
-  EasyFormTypes,
-  FormArray,
-  OnSubmit,
-  Hook,
-  UpdateFormArray,
-  UpdateDefaultValues,
-  SetValueManually,
-  SetErrorManually,
-  RunValidate,
-  UpdateEvent,
-  ResetEvent,
-  GetProps,
-  MultipleFieldUpdate,
+//   EasyFormTypes,
+//   FormArray,
+    FormItem,
+//   OnSubmit,
+//   Hook,
+//   UpdateFormArray,
+//   UpdateDefaultValues,
+//   SetValueManually,
+//   SetErrorManually,
+//   RunValidate,
+//   UpdateEvent,
+//   ResetEvent,
+//   GetProps,
+//   MultipleFieldUpdate,
 } from './types';
 
-export const useEasyForm = <T extends Record<string, unknown>>({
-  initialForm,
-  resetAfterSubmit,
-}: EasyFormTypes<T>): Hook<T> => {
-  const updatedInitialForm = useRef<FormArray<T>>(
-    setPropertiesToForm(initialForm),
-  );
+export const useEasyForm = <T>({
+  defaultValues: {},
+}) => {
+  const form = useRef(new Map<string, FormItem<T>>());
+  // const updatedInitialForm = useRef<FormArray<T>>(
+  //   setPropertiesToForm(initialForm),
+  // );
 
-  const [formArray, setFormArray] = useState<FormArray<T>>(
-    updatedInitialForm.current,
-  );
+  // const [formArray, setFormArray] = useState<FormArray<T>>(
+  //   updatedInitialForm.current,
+  // );
 
-  const formObject = useMemo(() => transformArrayToObject<T>(formArray), [
-    formArray,
-  ]);
+  // const formObject = useMemo(() => transformArrayToObject<T>(formArray), [
+  //   formArray,
+  // ]);
 
-  const disabled = useMemo<boolean>(() => checkRequiredProperty<T>(formArray), [
-    formArray,
-  ]);
+  // const disabled = useMemo<boolean>(() => checkRequiredProperty<T>(formArray), [
+  //   formArray,
+  // ]);
 
-  const pristine = useMemo<boolean>(() => {
-    return compareValues(updatedInitialForm.current, formArray);
-  }, [formArray]);
+  // const pristine = useMemo<boolean>(() => {
+  //   return compareValues(updatedInitialForm.current, formArray);
+  // }, [formArray]);
 
-  const valid = useMemo<boolean>(
-    () =>
-      !hasAnyErrorsInForm(formArray, getOtherValues(formArray)) &&
-      checkFormValid(formArray),
-    [formArray],
-  );
+  // const valid = useMemo<boolean>(
+  //   () =>
+  //     !hasAnyErrorsInForm(formArray, getOtherValues(formArray)) &&
+  //     checkFormValid(formArray),
+  //   [formArray],
+  // );
 
-  const resetEvent: ResetEvent = useCallback(() => {
-    setFormArray(updatedInitialForm.current);
-  }, []);
+  // const register = useCallback((name: keyof T & string, options) => {
+  //   console.log(name, options);
+  //   form.current.set(name, options);
+  // }, []);
 
-  const multipleFieldUpdate: MultipleFieldUpdate<T> = useCallback((fields) => {
-    setFormArray((ps) => multiUpdate(ps, fields));
-  }, []);
+  // const resetEvent: ResetEvent = useCallback(() => {
+  //   setFormArray(updatedInitialForm.current);
+  // }, []);
 
-  const updateDefaultValues: UpdateDefaultValues<T> = useCallback(
-    (v) => {
-      if (!v) resetEvent();
-      else multipleFieldUpdate(v);
-    },
-    [multipleFieldUpdate, resetEvent],
-  );
+  // const multipleFieldUpdate: MultipleFieldUpdate<T> = useCallback((fields) => {
+  //   setFormArray((ps) => multiUpdate(ps, fields));
+  // }, []);
 
-  /**
-   * please do not use that function
-   * will be removed in future
-   */
-  const _updateFormArray: UpdateFormArray<T> = useCallback((array) => {
-    if (!array || !Array.isArray(array)) return;
+  // const updateDefaultValues: UpdateDefaultValues<T> = useCallback(
+  //   (v) => {
+  //     if (!v) resetEvent();
+  //     else multipleFieldUpdate(v);
+  //   },
+  //   [multipleFieldUpdate, resetEvent],
+  // );
 
-    const newInitialForm = setPropertiesToForm(array);
-    updatedInitialForm.current = newInitialForm;
-    setFormArray(newInitialForm);
-  }, []);
+  // /**
+  //  * please do not use that function
+  //  * will be removed in future
+  //  */
+  // const _updateFormArray: UpdateFormArray<T> = useCallback((array) => {
+  //   if (!array || !Array.isArray(array)) return;
 
-  const runValidate: RunValidate<keyof T> = useCallback((name) => {
-    if (!name) return;
+  //   const newInitialForm = setPropertiesToForm(array);
+  //   updatedInitialForm.current = newInitialForm;
+  //   setFormArray(newInitialForm);
+  // }, []);
 
-    setFormArray((ps) =>
-      ps.map((filed) => {
-        if (filed.name === name) {
-          const otherValues = getOtherValues<T>(ps, filed.name);
-          const error = validator(filed.value, otherValues, filed.validate);
-          const isValidField = !!(!error && filed.touched);
+  // const runValidate: RunValidate<keyof T> = useCallback((name) => {
+  //   if (!name) return;
 
-          return { ...filed, error, touched: true, isValidField };
-        }
-        return filed;
-      }),
-    );
-  }, []);
+  //   setFormArray((ps) =>
+  //     ps.map((filed) => {
+  //       if (filed.name === name) {
+  //         const otherValues = getOtherValues<T>(ps, filed.name);
+  //         const error = validator(filed.value, otherValues, filed.validate);
+  //         const isValidField = !!(!error && filed.touched);
 
-  const setErrorManually: SetErrorManually<keyof T> = useCallback(
-    (name, error) => {
-      setFormArray((ps) =>
-        ps.map((filed) =>
-          filed.name === name
-            ? { ...filed, touched: true, error, isValidField: false }
-            : filed,
-        ),
-      );
-    },
-    [],
-  );
+  //         return { ...filed, error, touched: true, isValidField };
+  //       }
+  //       return filed;
+  //     }),
+  //   );
+  // }, []);
 
-  const setValueManually: SetValueManually<keyof T> = useCallback(
-    (name, value) => {
-      setFormArray((ps) => {
-        const newForm = ps.map((filed) => {
-          if (filed.name === name) {
-            return { ...filed, value, touched: true, error: '' };
-          }
-          return filed;
-        });
+  // const setErrorManually: SetErrorManually<keyof T> = useCallback(
+  //   (name, error) => {
+  //     setFormArray((ps) =>
+  //       ps.map((filed) =>
+  //         filed.name === name
+  //           ? { ...filed, touched: true, error, isValidField: false }
+  //           : filed,
+  //       ),
+  //     );
+  //   },
+  //   [],
+  // );
 
-        return newForm.map((filed) => {
-          if (!filed.onChangeValidate) return filed;
+  // const setValueManually: SetValueManually<keyof T> = useCallback(
+  //   (name, value) => {
+  //     setFormArray((ps) => {
+  //       const newForm = ps.map((filed) => {
+  //         if (filed.name === name) {
+  //           return { ...filed, value, touched: true, error: '' };
+  //         }
+  //         return filed;
+  //       });
 
-          const otherValues = getOtherValues(newForm, filed.name);
-          const error = validator(filed.value, otherValues, filed.validate);
-          const isValidField = !!(!error && filed.touched);
-          return { ...filed, error, isValidField };
-        });
-      });
-    },
-    [],
-  );
+  //       return newForm.map((filed) => {
+  //         if (!filed.onChangeValidate) return filed;
 
-  const updateEvent: UpdateEvent = useCallback(
-    (e) => {
-      if (!e || !e.target) return;
-      const { value, type, checked, name } = e.target;
-      const v = type === 'checkbox' ? checked : value;
+  //         const otherValues = getOtherValues(newForm, filed.name);
+  //         const error = validator(filed.value, otherValues, filed.validate);
+  //         const isValidField = !!(!error && filed.touched);
+  //         return { ...filed, error, isValidField };
+  //       });
+  //     });
+  //   },
+  //   [],
+  // );
 
-      setValueManually(name, v);
-    },
-    [setValueManually],
-  );
+  // const updateEvent: UpdateEvent = useCallback(
+  //   (e) => {
+  //     if (!e || !e.target) return;
+  //     const { value, type, checked, name } = e.target;
+  //     const v = type === 'checkbox' ? checked : value;
 
-  const submitEvent: OnSubmit<T> = useCallback(
-    (callback) => async (e) => {
-      if (e) {
-        e.preventDefault();
-        e.persist();
-      }
+  //     setValueManually(name, v);
+  //   },
+  //   [setValueManually],
+  // );
 
-      const otherValues = getOtherValues(formArray);
-      const hasAnyErrorInForm = hasAnyErrorsInForm(formArray, otherValues);
-      if (hasAnyErrorInForm) {
-        setFormArray(
-          formArray.map((filed) => {
-            const error =
-              filed.error ||
-              validator(filed.value, otherValues, filed.validate);
-            const isValidField = !error;
-            return {
-              ...filed,
-              touched: true,
-              error: filed.error
-                ? filed.error
-                : validator(filed.value, otherValues, filed.validate),
-              isValidField,
-            };
-          }),
-        );
-        return;
-      }
+  // const submitEvent: OnSubmit<T> = useCallback(
+  //   (callback) => async (e) => {
+  //     if (e) {
+  //       e.preventDefault();
+  //       e.persist();
+  //     }
 
-      const data = getOutputObject<T>(formArray);
-      await callback(data, e);
-      if (resetAfterSubmit) resetEvent();
-    },
-    [resetEvent, resetAfterSubmit, formArray],
-  );
+  //     const otherValues = getOtherValues(formArray);
+  //     const hasAnyErrorInForm = hasAnyErrorsInForm(formArray, otherValues);
+  //     if (hasAnyErrorInForm) {
+  //       setFormArray(
+  //         formArray.map((filed) => {
+  //           const error =
+  //             filed.error ||
+  //             validator(filed.value, otherValues, filed.validate);
+  //           const isValidField = !error;
+  //           return {
+  //             ...filed,
+  //             touched: true,
+  //             error: filed.error
+  //               ? filed.error
+  //               : validator(filed.value, otherValues, filed.validate),
+  //             isValidField,
+  //           };
+  //         }),
+  //       );
+  //       return;
+  //     }
 
-  const getProps: GetProps<T, keyof T> = useCallback(
-    (n, rest, onlyValidDomAttr = false) => {
-      const element = formArray.find((filed) => filed.name === n);
-      if (!element) return { onChange: updateEvent, ...rest };
+  //     const data = getOutputObject<T>(formArray);
+  //     await callback(data, e);
+  //     if (resetAfterSubmit) resetEvent();
+  //   },
+  //   [resetEvent, resetAfterSubmit, formArray],
+  // );
 
-      const { name, value, touched, error } = element;
+  // const getProps: GetProps<T, keyof T> = useCallback(
+  //   (n, rest, onlyValidDomAttr = false) => {
+  //     const element = formArray.find((filed) => filed.name === n);
+  //     if (!element) return { onChange: updateEvent, ...rest };
 
-      if (onlyValidDomAttr) {
-        return {
-          name,
-          value,
-          onChange: updateEvent,
-          ...rest,
-        };
-      }
+  //     const { name, value, touched, error } = element;
 
-      return {
-        name,
-        value,
-        onChange: updateEvent,
-        touched,
-        error,
-        ...rest,
-      };
-    },
-    [updateEvent, formArray],
-  );
+  //     if (onlyValidDomAttr) {
+  //       return {
+  //         name,
+  //         value,
+  //         onChange: updateEvent,
+  //         ...rest,
+  //       };
+  //     }
 
-  return {
-    formArray,
-    formObject,
-    resetEvent: resetEvent,
-    updateEvent: updateEvent,
-    setErrorManually: setErrorManually,
-    setValueManually: setValueManually,
-    multipleFieldUpdate: multipleFieldUpdate,
-    updateDefaultValues: updateDefaultValues,
-    _updateFormArray: _updateFormArray,
-    runValidate: runValidate,
-    submitEvent: submitEvent,
-    getProps: getProps,
-    pristine,
-    valid,
-    disabled,
-  };
+  //     return {
+  //       name,
+  //       value,
+  //       onChange: updateEvent,
+  //       touched,
+  //       error,
+  //       ...rest,
+  //     };
+  //   },
+  //   [updateEvent, formArray],
+  // );
+
+  // return {
+  //   formArray,
+  //   formObject,
+  //   resetEvent: resetEvent,
+  //   updateEvent: updateEvent,
+  //   setErrorManually: setErrorManually,
+  //   setValueManually: setValueManually,
+  //   multipleFieldUpdate: multipleFieldUpdate,
+  //   updateDefaultValues: updateDefaultValues,
+  //   _updateFormArray: _updateFormArray,
+  //   runValidate: runValidate,
+  //   submitEvent: submitEvent,
+  //   getProps: getProps,
+  //   pristine,
+  //   valid,
+  //   disabled,
+  // };
 };
